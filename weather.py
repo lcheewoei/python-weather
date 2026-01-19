71af53f582dda861ebfe3477b5cd011658e6945a
Chee-Woei <lcheewoei@yahoo.com>
2026-01-19 05:25:47 -0800

    first commit

---
+++ weather.py
@@ -0,0 +1,28 @@
+import requests
+from dotenv import load_dotenv
+from pprint import pprint
+import os
+
+load_dotenv()
+
+
+def get_current_weather(city="Los Gatos"):
+
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+
+if __name__ == "__main__":
+    print("\n***Get Current Weather Conditions ***\n")
+    city = input("Please enter a city name:\n")
+
+    # check for empty string or string with only spaces
+    if not bool(city.strip()):
+        city = "Kansas City"
+
+    weather_data = get_current_weather(city)
+    print("\n")
+    pprint(weather_data)
